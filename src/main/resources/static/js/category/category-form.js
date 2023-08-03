9200fdc1cde50c1c3aadcb7dfadb592130bba062
masarunakanofox <135203028+masarunakanofox@users.noreply.github.com>
2023-08-03 19:27:23 +0900

    N0.19

--- src/main/resources/static/js/category/category-form.js
+++ src/main/resources/static/js/category/category-form.js
@@ -7,50 +7,46 @@ $(document).ready(function () {
   });
 
   function validateForm() {
-    var isValid = true;
-    $(".form-control").each(function () {
-      let id = $(this).attr("id");
+    let isCodeValid = true;
+    let isNameValid = true;
+    let isDisplayOrderValid = true;
 
-      // codeのバリデーション
-      if (id === "code") {
-        let code = $(this).val();
-        if (code === "" || !isPatternValid(code)) {
-          $(this).addClass("is-invalid");
-          isValid = false;
-        } else {
-          $(this).removeClass("is-invalid");
-        }
-        console.log(!isPatternValid(code));
-      }
+    // codeのバリデーション
+    const codeInput = $("#code");
+    const codeValue = codeInput.val().trim();
+    const codePattern = /^[A-Z]{3}-\d{3}$/;
 
-      // nameのバリデーション
-      if (id === "name") {
-        let name = $(this).val();
-        if (name === "" || name.length >= 20) {
-          $(this).addClass("is-invalid");
-          isValid = false;
-        } else {
-          $(this).removeClass("is-invalid");
-        }
-        console.log(name.length);
-      }
+    if (!codePattern.test(codeValue)) {
+      codeInput.addClass("is-invalid");
+      isCodeValid = false;
+    } else {
+      codeInput.removeClass("is-invalid");
+    }
 
-      // desplay_orderのバリデーション
-      if (id === "display_order") {
-        let displayOrder = $(this).val();
-        if (displayOrder === "" || displayOrder < 0 || displayOrder > 999) {
-          $(this).addClass("is-invalid");
-          isValid = false;
-        } else {
-          $(this).removeClass("is-invalid");
-        }
-      }
-    });
+    // nameのバリデーション
+    const nameInput = $("#name");
+    const nameValue = nameInput.val();
+
+    if (nameValue === "" || nameValue.length > 20) {
+      nameInput.addClass("is-invalid");
+      isNameValid = false;
+    } else {
+      nameInput.removeClass("is-invalid");
+    }
+
+    // display_orderのバリデーション
+    const displayOrderInput = $("#display_order");
+    const displayOrderValue = displayOrderInput.val();
+
+    if (displayOrderValue === "" || isNaN(displayOrderValue) || displayOrderValue < 0 || displayOrderValue > 999) {
+      displayOrderInput.addClass("is-invalid");
+      isDisplayOrderValid = false;
+    } else {
+      displayOrderInput.removeClass("is-invalid");
+    }
+
+    // 最終的なバリデーション結果をまとめる
+    const isValid = isCodeValid && isNameValid && isDisplayOrderValid;
     return isValid;
-  }
-
-  function isPatternValid(code) {
-    let pattern = /^([A-Z]{3}[0-9]{3})$/;
-    return pattern.test(code);
   }
 });
